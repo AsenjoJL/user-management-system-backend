206edafd12e21196423633751a745296d41c4e41
AsenjoJL <asenjojohnlester24@gmail.com>
2025-05-18 19:40:55 +0800

    Updated

--- node-mysql-boilerplate-api/accounts/account.service.js
+++ node-mysql-boilerplate-api/accounts/account.service.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs');
 const crypto = require('crypto');
 const { Op } = require('sequelize');
 const sendEmail = require('_helpers/send-email');
-const db = require('_helpers/db');
+const db = require('../_helpers/db');
 const Role = require('_helpers/role');
 
 module.exports = {
