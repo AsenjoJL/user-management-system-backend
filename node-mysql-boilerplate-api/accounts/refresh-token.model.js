b273e62f983bceb57944ebfe7b822ba1558ed231
AsenjoJL <asenjojohnlester24@gmail.com>
2025-05-18 22:54:21 +0800

    Updated

--- node-mysql-boilerplate-api/accounts/refresh-token.model.js
+++ node-mysql-boilerplate-api/accounts/refresh-token.model.js
@@ -4,27 +4,43 @@ module.exports = model;
 
 function model(sequelize) {
     const attributes = {
-        token: { type: DataTypes.STRING },
-        expires: { type: DataTypes.DATE },
+        token: { type: DataTypes.STRING, allowNull: false },
+        expires: { type: DataTypes.DATE, allowNull: false },
         created: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
-        createdByIp: { type: DataTypes.STRING },
+        createdByIp: { type: DataTypes.STRING, allowNull: false },
         revoked: { type: DataTypes.DATE },
         revokedByIp: { type: DataTypes.STRING },
         replacedByToken: { type: DataTypes.STRING },
         isExpired: {
             type: DataTypes.VIRTUAL,
-            get() { return Date.now() >= this.expires; }
+            get() {
+                return Date.now() >= new Date(this.expires).getTime();
+            }
         },
         isActive: {
             type: DataTypes.VIRTUAL,
-            get() { return !this.revoked && !this.isExpired; }
+            get() {
+                return !this.revoked && !this.isExpired;
+            }
         }
     };
 
     const options = {
-        // disable default timestamp fields (createdAt and updatedAt)
-        timestamps: false
+        timestamps: false,
+        defaultScope: {
+            attributes: { exclude: ['createdByIp', 'revokedByIp', 'replacedByToken'] }
+        }
     };
 
-    return sequelize.define('refreshToken', attributes, options);
+    const RefreshToken = sequelize.define('refreshToken', attributes, options);
+
+    // Make sure RefreshToken is associated with the User model (usually via foreign key)
+    RefreshToken.associate = function(models) {
+        RefreshToken.belongsTo(models.Account, {
+            foreignKey: 'accountId',
+            onDelete: 'CASCADE'
+        });
+    };
+
+    return RefreshToken;
 }
