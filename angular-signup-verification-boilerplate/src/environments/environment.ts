b9ac79c66f17d41a33260dee4620c2933d025c77
AsenjoJL <asenjojohnlester24@gmail.com>
2025-05-18 16:52:03 +0800

    Updated

--- angular-signup-verification-boilerplate/src/environments/environment.ts
+++ angular-signup-verification-boilerplate/src/environments/environment.ts
@@ -2,10 +2,12 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// environment.ts (dev)
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:4000'
+  apiUrl: 'http://localhost:3000' // or your local backend URL
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
