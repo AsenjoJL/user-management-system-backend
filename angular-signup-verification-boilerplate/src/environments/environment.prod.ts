eb575e927061714e117fc90aa0134b525fc2bf1a
jrome09 <chuyaxxjerome.com>
2025-05-19 17:31:37 +0800

    Updated Config & environment

--- angular-signup-verification-boilerplate/src/environments/environment.prod.ts
+++ angular-signup-verification-boilerplate/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl:
-    'https://user-management-system-backend-production-fc9b.up.railway.app',
+  apiUrl: 'https://user-management-api-production-c4e5.up.railway.app',
 };
